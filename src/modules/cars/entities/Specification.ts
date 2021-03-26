07b285b0ef9dee124a692e6c360079dd3ce4945a
diego64 <diegoferreira1964@gmail.com>
2021-03-26 01:11:25 -0300

    Criacao da migration de especificacao e continuacao da documentacao

--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,12 +1,21 @@
 /* eslint-disable prettier/prettier */
 // eslint-disable-next-line prettier/prettier
 
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
 
+@Entity("specifications")
 class Specification {
+    @PrimaryColumn()
     id?: string;
+
+    @Column()
     name: string;
+    
+    @Column()
     description: string;
+
+    @CreateDateColumn()
     created_at: Date;
 
     constructor() {
