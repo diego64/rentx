e96e6185db2ed1cc5bee78bb89faaba6180c539b
diego64 <diegoferreira1964@gmail.com>
2021-04-08 20:43:09 -0300

    Iniciando o serviço de esqueci a senha

---
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -0,0 +1,39 @@
+import { inject, injectable } from "tsyringe";
+import { v4 as uuidV4 } from "uuid";
+
+import { IUsersRsepository } from "@modules/accounts/repositories/IUsersRepository";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { AppError } from "@shared/erros/AppError";
+import { IDateProvider } from "@shared/container/provaiders/DateProvaider/IDateProvider";
+
+@injectable()
+class SendForgotPasswordMailUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRsepository,
+        @inject("UsersTokensRepository")
+        private UsersTokensRepository: IUsersTokensRepository,
+        @inject("DayjsDateProvaider")
+        private dateProvider: IDateProvider
+    ) {}
+
+    async execute(email: string) {
+        const user = await this.usersRepository.findByEmail(email);
+
+        if(!user) {
+            throw new AppError("Users does not exists!")
+        }
+
+        const token = uuidV4();
+
+        const expires_date = this.dateProvider.addHours(3);
+
+        await this.UsersTokensRepository.create({
+            refresh_token: token,
+            user_id: user.id,
+            expires_date,
+        })
+    }
+}
+
+export { SendForgotPasswordMailUseCase }
