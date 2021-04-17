0be2190039e69ea547f9ead9adcf1f6b9ff12ca9
diego64 <diegoferreira1964@gmail.com>
2021-04-16 22:51:40 -0300

    Configurando o upload para a AWS

--- src/shared/container/provaiders/index.ts
+++ src/shared/container/provaiders/index.ts
@@ -5,6 +5,7 @@ import { DayjsDateProvaider } from "./DateProvaider/implementations/DayjsDatePro
 import { EtherealMailProvider } from "./implementations/EtherealMailProvider";
 import { IMailProvider } from "./MailProvider/IMailProvider";
 import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
+import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
 import { IStorageProvider } from "./StorageProvider/IStorageProvider";
 
 container.registerSingleton<IDateProvider>(
@@ -17,7 +18,12 @@ container.registerInstance<IMailProvider>(
   new EtherealMailProvider()
 );
 
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider,
+};
+
 container.registerSingleton<IStorageProvider>(
   "StorageProvider",
-  LocalStorageProvider
+  diskStorage[process.env.disk]
 );
